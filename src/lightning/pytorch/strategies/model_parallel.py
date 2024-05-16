ba23bfcc6a6c8f692f8b27f68848ae2e55465baf
awaelchli <aedu.waelchli@gmail.com>
2024-05-16 14:12:39 +0200

    checkpointing unimplemented

--- src/lightning/pytorch/strategies/model_parallel.py
+++ src/lightning/pytorch/strategies/model_parallel.py
@@ -11,11 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import shutil
 from contextlib import contextmanager, nullcontext
 from datetime import timedelta
-from pathlib import Path
-from typing import TYPE_CHECKING, Any, Dict, Generator, List, Literal, Mapping, Optional, Set, Type, Union
+from typing import TYPE_CHECKING, Any, Dict, Generator, List, Literal, Mapping, Optional, Union
 
 import torch
 from lightning_utilities.core.rank_zero import rank_zero_only as utils_rank_zero_only
@@ -26,12 +24,7 @@ from typing_extensions import override
 import lightning.pytorch as pl
 from lightning.fabric.plugins import CheckpointIO
 from lightning.fabric.plugins.collectives.torch_collective import default_pg_timeout
-from lightning.fabric.strategies.fsdp import (
-    _METADATA_FILENAME,
-    _distributed_checkpoint_save,
-    _is_sharded_checkpoint,
-)
-from lightning.fabric.strategies.model_parallel import _load_checkpoint, _setup_device_mesh
+from lightning.fabric.strategies.model_parallel import _setup_device_mesh
 from lightning.fabric.utilities.distributed import (
     _distributed_is_initialized,
     _get_default_process_group_backend_for_device,
@@ -191,31 +184,7 @@ class ModelParallelStrategy(ParallelStrategy):
         # and subsequent checkpoint saving can fail
         self._reset_optimizers_and_schedulers()
 
-        # TODO:
-        # if self.kwargs.get("use_orig_params"):
-        #     return super().setup_optimizers(trainer)
-
-        invalid_params_error = False
-        # try:
-        #     # In PyTorch < 2.0, or if `use_orig_params=False` the user needs to do access
-        #     # `self.trainer.model.parameters()` in configure_optimizers()
         return super().setup_optimizers(trainer)
-        # except ValueError as ex:
-        #     if "optimizer got an empty parameter list" not in str(ex):
-        #         raise
-        #     invalid_params_error = True
-
-        # if invalid_params_error or any(not _optimizer_has_flat_params(optimizer) for optimizer in self.optimizers):
-        #     # We avoid this limitation in PyTorch >= 2.0 by setting `use_orig_params=True`
-            
-        #     print(invalid_params_error,  any(not _optimizer_has_flat_params(optimizer) for optimizer in self.optimizers))
-            
-        #     raise ValueError(
-        #         "The optimizer does not seem to reference any FSDP parameters. HINT: Make sure to create the"
-        #         " optimizer after setting up the model by referencing `self.trainer.model.parameters()` in the"
-        #         " `configure_optimizers()` hook."
-        #     )
-        return None
 
     @override
     def model_to_device(self) -> None:
@@ -308,134 +277,11 @@ class ModelParallelStrategy(ParallelStrategy):
     def save_checkpoint(
         self, checkpoint: Dict[str, Any], filepath: _PATH, storage_options: Optional[Any] = None
     ) -> None:
-        if storage_options is not None:
-            raise TypeError(
-                f"`{type(self).__name__}.save_checkpoint(..., storage_options=...)` is not supported because"
-                f" `{type(self).__name__}` does not use the `CheckpointIO`."
-            )
-
-        path = Path(self.broadcast(filepath))
-        if path.is_dir() and not self._save_distributed_checkpoint and not _is_sharded_checkpoint(path):
-            raise IsADirectoryError(f"The checkpoint path exists and is a directory: {path}")
-
-        if self._save_distributed_checkpoint:
-            if path.is_file():
-                path.unlink()
-            path.mkdir(parents=True, exist_ok=True)
-
-            converted_state = {"model": checkpoint.pop("state_dict")}
-            converted_state.update({
-                f"optimizer_{idx}": optim_state
-                for idx, optim_state in enumerate(checkpoint.pop("optimizer_states", []))
-            })
-
-            _distributed_checkpoint_save(converted_state, path)
-
-            if self.global_rank == 0:
-                torch.save(checkpoint, path / _METADATA_FILENAME)
-        else:
-            if _is_sharded_checkpoint(path):
-                shutil.rmtree(path)
-            return super().save_checkpoint(checkpoint=checkpoint, filepath=path)
+        raise NotImplementedError("Checkpoint saving is not yet implemented.")
 
     @override
     def load_checkpoint(self, checkpoint_path: _PATH) -> Dict[str, Any]:
-        # broadcast the path from rank 0 to ensure all the states are loaded from a common path
-        path = Path(self.broadcast(checkpoint_path))
-
-        checkpoint = torch.load(checkpoint_path, mmap=True, map_location="cpu")
-
-        state = {}  # ?
-        checkpoint = _load_checkpoint(path=path, state=state, strict=self.lightning_module.strict_loading)
-        return checkpoint
-
-        # from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-        #
-        # assert self.model is not None
-        # assert self.lightning_module is not None
-        #
-        # if _is_sharded_checkpoint(path):
-        #     from torch.distributed.checkpoint.optimizer import load_sharded_optimizer_state_dict
-        #
-        #     state_dict_ctx = _get_sharded_state_dict_context(self.model)
-        #
-        #     with state_dict_ctx:
-        #         module_state = {"model": self.model.state_dict()}
-        #         _distributed_checkpoint_load(module_state, path)
-        #         self.model.load_state_dict(module_state["model"], strict=self.lightning_module.strict_loading)
-        #
-        #         if self.lightning_module.trainer.state.fn == TrainerFn.FITTING and self.optimizers:
-        #             from torch.distributed.checkpoint import FileSystemReader
-        #
-        #             # TODO: replace with newer APIs
-        #             # https://github.com/pytorch/pytorch/issues/119800#issuecomment-1942156271
-        #             reader = FileSystemReader(path=path)
-        #             # the optimizer states must be loaded separately
-        #             for idx, optim in enumerate(self.optimizers):
-        #                 optim_key = f"optimizer_{idx}"
-        #                 optim_state = load_sharded_optimizer_state_dict(
-        #                     model_state_dict=module_state["model"],
-        #                     optimizer_key=optim_key,
-        #                     storage_reader=reader,
-        #                 )
-        #                 flattened_osd = FSDP.optim_state_dict_to_load(
-        #                     optim_state_dict=optim_state[optim_key],
-        #                     model=self.model,
-        #                     optim=optim,
-        #                 )
-        #                 optim.load_state_dict(flattened_osd)
-        #
-        #     # Load metadata (anything not a module or optimizer)
-        #     metadata = torch.load(path / _METADATA_FILENAME)
-        #     return metadata
-        #
-        # if _is_full_checkpoint(path):
-        #     checkpoint = _lazy_load(path)
-        #     _load_raw_module_state(
-        #         checkpoint.pop("state_dict"),
-        #         module=self.model,
-        #         world_size=self.world_size,
-        #         strict=self.lightning_module.strict_loading,
-        #     )
-        #
-        #     # Materialize lazy tensors if there are any left in the checkpoint
-        #     # The `torch.Optimizer.load_state_dict` method can't load lazy tensors because of deepcopy pickle issues
-        #     checkpoint = _materialize_tensors(checkpoint)
-        #
-        #     from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-        #     from torch.distributed.fsdp import OptimStateKeyType
-        #
-        #     optimizer_states = checkpoint.get("optimizer_states")
-        #     if optimizer_states is None or self.lightning_module.trainer.state.fn != TrainerFn.FITTING:
-        #         # If the optimizer states are not present, we don't need to do anything (backward compatibility)
-        #         return checkpoint
-        #     if len(self.optimizers) != len(optimizer_states):
-        #         raise RuntimeError(
-        #             f"You have configured {len(self.optimizers)} optimizers but the checkpoint contains"
-        #             f" {len(optimizer_states)} optimizers to load. Please resume training with the same number"
-        #             " of optimizers or edit the checkpoint manually to remove states."
-        #         )
-        #
-        #     # rank0_only should be false because we need to load the optimizer state on all ranks
-        #     with _get_full_state_dict_context(self.model, world_size=self.world_size, rank0_only=False):
-        #         for optimizer, opt_state in zip(self.optimizers, optimizer_states):
-        #             if isinstance(list(opt_state["state"].keys())[0], int):
-        #                 # Handling the case where the optimizer state is saved from a normal optimizer
-        #                 opt_state = FSDP.rekey_optim_state_dict(opt_state, OptimStateKeyType.PARAM_NAME, self.model)
-        #
-        #             opt_state = FSDP.optim_state_dict_to_load(
-        #                 optim_state_dict=opt_state,
-        #                 model=self.model,
-        #                 optim=optimizer,
-        #             )
-        #             optimizer.load_state_dict(opt_state)
-        #
-        #     return checkpoint
-        #
-        # raise ValueError(
-        #     f"The path {str(path)!r} does not point to a valid checkpoint. Make sure the path points to either a"
-        #     " directory with FSDP checkpoint shards, or a single file with a full checkpoint."
-        # )
+        raise NotImplementedError("Checkpoint loading is not yet implemented.")
 
     def _setup_distributed(self) -> None:
         super().setup_environment()
